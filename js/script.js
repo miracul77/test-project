84c7817831b30855c36248e586b9a01b80e7dce2
Vladimir <123245991+miracul77@users.noreply.github.com>
2023-07-19 04:12:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 window.addEventListener('load', () => {
   const menu = document.querySelector('.nav'),
     menuItem = document.querySelectorAll('.nav__item'),
-    hamburger = document.querySelector('.hamburger');
+    hamburger = document.querySelector('.hamburger'),
+    carousel = document.querySelector('.carousel'),
+    arrowBtns = document.querySelectorAll('.slider-arrow'),
+    firstCard = carousel.querySelector('.firm').offsetWidth;
 
   hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('hamburger_active');
@@ -12,6 +15,12 @@ window.addEventListener('load', () => {
     item.addEventListener('click', () => {
       hamburger.classList.toggle('hamburger_active');
       menu.classList.toggle('nav_active');
+    });
+  });
+
+  arrowBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      carousel.scrollLeft += btn.id == 'left' ? -firstCard : firstCard;
     });
   });
 });
